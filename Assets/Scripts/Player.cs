804d8fe4a318be47de68db7d3d15e19a82675755
Robert Kuropkat <robert@kuropkat.com>
2024-09-25 14:00:40 -0400

    Ammo PowerUp and incremental commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,41 +9,36 @@ public class Player : MonoBehaviour
     //
     [SerializeField] private float        mySpeed           = 3.5f;
     [SerializeField] private int          speedUp           = 0;
-    [SerializeField] private float        fireRate          = .15f;
-    [SerializeField] private float        tripleShotTimer   = 5f;
     [SerializeField] private float        speedUpTimer      = 5f;
     [SerializeField] private float        leftRightBoundary = 11.2f;
     [SerializeField] private float        topBoundary       = 0;
     [SerializeField] private float        bottomBoundary    = -4;
     [SerializeField] private float        ThrusterIncrease  = 1.1f;
-    [SerializeField] private Vector3      laserOffest       = new Vector3(0, 1.006f, 0);
     //
     // Game Counters
     //
     [SerializeField] private int          playerLives       = 3;
     [SerializeField] private int          playerScore       = 0;
-    [SerializeField] private int          ammoCount         = 16;
-    //
-    // Flags
-    //
-    [SerializeField] private bool         laserCanFire      = true;
-    [SerializeField] private bool         tripleShot        = false;
     //
     // Game Objects populated in Inspector
     //
     [SerializeField] private SpawnManager spawnManager;
     [SerializeField] private UIManager    uiManager;
     [SerializeField] private GameManager  gameManager;
-    [SerializeField] private GameObject   laserPrefab;
-    [SerializeField] private GameObject   tripleShotPrefab;
     [SerializeField] private Shields      shieldAnim;
     [SerializeField] private Animator     myExplosion;
+    [SerializeField] private Weapons      myWeapons;
     [SerializeField] private GameObject[] fireEngineAnims;
     //
     // Properties
     //
     private bool PlayerHasFired  
-        { get { return Input.GetKeyDown(KeyCode.Space); } }
+        { get {
+                if (  Input.GetKeyDown(KeyCode.Space) 
+                   && playerLives > 0) { return true; } 
+                return false;
+              } 
+        }
 
     private int ChooseEngine  // The engine fire animations are stored in an array
     {                         // This property randomizes which one is chosen first
@@ -72,8 +67,6 @@ public class Player : MonoBehaviour
     {
         //ToDo:  Add actual error handling rather than just a debug message.
         if (spawnManager     == null) { Debug.LogError("The Spawn Manager is NULL."); }
-        if (laserPrefab      == null) { Debug.LogError("The Laser Prefab is NULL."); }
-        if (tripleShotPrefab == null) { Debug.LogError("The TripleShot PowerUp Prefab is NULL"); }
         if (shieldAnim       == null) { Debug.LogError("The Shield Animation is NULL"); }
         if (uiManager        == null) { Debug.LogError("The UI Manager is NULL"); }
         if (gameManager      == null) { Debug.LogError("Game Manager is NULL"); }
@@ -82,7 +75,6 @@ public class Player : MonoBehaviour
     private void Start()
     {
         NullCheckOnStartup();
-        uiManager.AmmoCount(ammoCount);
         transform.position = new Vector3(0, 0, 0);
     }
 
@@ -90,10 +82,7 @@ public class Player : MonoBehaviour
     {
         MovePlayer();
         CheckBoundaries();
-
-        if (  PlayerHasFired  // check order.  For fast fail.  Again, a useful optimization!!!
-           && laserCanFire 
-           ) { FireLaser(); }
+        if (PlayerHasFired) { myWeapons.FireLaser(); }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -109,13 +98,16 @@ public class Player : MonoBehaviour
                 TakeDamage();
                 break;
             case "TripleShotPU":
-                if (!tripleShot) { StartCoroutine(PowerUpTripleShot()); }
+                if (!myWeapons.TripleShotEnabled) { myWeapons.TripleShotEnabled = true; }
                 break;
             case "SpeedPU":
                 if (speedUp == 0) { StartCoroutine(PowerUpSpeed()); }
                 break;
             case "ShieldPU":
                 if (!shieldAnim.IsActive) { shieldAnim.IsActive = true; }
+                break;
+            case "AmmoPU":
+                myWeapons.CollectAmmo();
                 break;
             default:
                 break;
@@ -126,29 +118,12 @@ public class Player : MonoBehaviour
     // Watchdogs            ============================================================
     //
 
-    private IEnumerator LaserCoolDown()
-    {
-        yield return new WaitForSeconds(fireRate);
-        laserCanFire = true;
-    }
-
-    private IEnumerator PowerUpTripleShot()
-    {
-        tripleShot = true;
-        yield return new WaitForSeconds(tripleShotTimer);
-        tripleShot = false;
-    }
-
     private IEnumerator PowerUpSpeed()
     {
         speedUp = 3;  // ToDo: This value should be moved up so it can be changed in the inspector
         yield return new WaitForSeconds(speedUpTimer);
         speedUp = 0;
     }
-
-    //
-    // Helper Methods           ============================================================
-    //
 
     //
     // Movement Methods
@@ -179,20 +154,6 @@ public class Player : MonoBehaviour
     // Damage Methods
     //
 
-    private void FireLaser()
-    {
-        if (playerLives < 1) { return; }
-        if (ammoCount   < 1) { return; }
-        ammoCount--;
-        uiManager.AmmoCount(ammoCount);
-
-        Instantiate((tripleShot) ? tripleShotPrefab : laserPrefab
-                   , transform.position + laserOffest
-                   , Quaternion.identity);
-        laserCanFire = false;
-        StartCoroutine(LaserCoolDown());
-    }
-
     public void EnemyDestroyed()
     {
         playerScore++;
@@ -201,14 +162,6 @@ public class Player : MonoBehaviour
 
     private void TakeDamage()
     {
-        /*
-        if (shieldAnim.IsActive)
-        {
-            //shieldAnim.TakesHit();
-            return;
-        }
-        */
-
         playerLives--;
         uiManager.CurrentLives(playerLives);            // report current lives count to dashboard
         if (playerLives < 1) { DeathScene(); return; }
---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    //
+    // Speed, Boundaries and Timers
+    //
+    [SerializeField] private float   fireRate        = .15f;
+    [SerializeField] private float   tripleShotTimer = 5f;
+    [SerializeField] private int     ammoCollectible = 15;
+    [SerializeField] private int     ammoStartCount  = 26;
+    [SerializeField] private Vector3 laserOffest     = new Vector3(0, 1.006f, 0);
+    //
+    // Flags
+    //
+    [SerializeField] private bool laserCanFire       = true;
+    //
+    // Game Objects populated in Inspector
+    //
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] private GameObject tripleShotPrefab;
+    [SerializeField] private UIManager  uiManager;
+    //
+    // Properties
+    //
+    [SerializeField] private int ammoCount;
+    private int AmmoCount
+    {
+        get { return ammoCount; }
+        set { 
+              ammoCount = value;
+              ammoCount = (ammoCount < 0) ? 0 : ammoCount;
+              uiManager.AmmoCount(AmmoCount);
+            }
+    }
+
+    [SerializeField] private bool tripleShotEnabled = false;
+    public bool TripleShotEnabled
+    {
+        get { return tripleShotEnabled;  }
+        set { 
+                tripleShotEnabled = value; 
+                StartCoroutine(PowerUpTripleShot());
+            }
+    }
+
+    private void NullCheckOnStartup()
+    {
+        if (laserPrefab      == null) { Debug.LogError("The Laser Prefab is NULL."); }
+        if (tripleShotPrefab == null) { Debug.LogError("The TripleShot PowerUp Prefab is NULL"); }
+        if (uiManager        == null) { Debug.LogError("The UI Manager is NULL"); }
+    }
+
+    void Start()
+    {
+        NullCheckOnStartup();
+        AmmoCount = ammoStartCount;
+    }
+
+    //
+    // Public Methods
+    //
+    public void FireLaser()
+    {
+        if (!laserCanFire) { return; }
+        if (AmmoCount < 1) { return; }
+        if (TripleShotEnabled) 
+             { AmmoCount = AmmoCount - 3; }
+        else { AmmoCount = AmmoCount - 1; }
+
+        Instantiate((TripleShotEnabled) ? tripleShotPrefab : laserPrefab
+                   , transform.position + laserOffest
+                   , Quaternion.identity);
+        laserCanFire = false;
+        StartCoroutine(LaserCoolDown());
+    }
+
+    public void CollectAmmo() { AmmoCount = AmmoCount + ammoCollectible; }
+
+    //
+    // Watchdogs            ============================================================
+    //
+
+    private IEnumerator LaserCoolDown()
+    {
+        yield return new WaitForSeconds(fireRate);
+        laserCanFire = true;
+    }
+
+    private IEnumerator PowerUpTripleShot()
+    {
+        yield return new WaitForSeconds(tripleShotTimer);
+        TripleShotEnabled = false;
+    }
+}
